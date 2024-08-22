5e26105c7b3ce96685a0fdcc467aef141dc4d902
Mark Miro <contact@markmiro.com>
2024-08-22 13:06:22 -0700

    Add shadcnui button and demo page

---
+++ app/demo/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Demo() {
+  return (
+    <div>
+      <h1>Demo</h1>
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+    </div>
+  );
+}
