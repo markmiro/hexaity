21767225a35699895ec946317dccec5796917e4c
Mark Miro <contact@markmiro.com>
2024-08-22 08:47:58 -0700

    Add people

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import { metadata } from "./layout";
 
 export default function Home() {
   const bgUrl = "/bg.jpg";
+  const avatarSize = 60;
+
   return (
     <>
       <Image
         src={bgUrl}
         loading="lazy"
-        className="fixed top-0 left-0 object-cover"
+        className="fixed top-0 left-0 h-full object-cover bg-black pointer-events-none"
         width={1920}
         height={1080}
         alt=""
@@ -24,20 +26,49 @@ export default function Home() {
             alt={metadata.title}
             priority
           />
-          <div className="pt-3" />
-          <p className="text-[11px] opacity-50 pb-2 text-center">
-            fullstack web development, product discovery,
-            <br />
-            rapid prototyping, WebGL/GLSL, Unity
-          </p>
         </header>
+        <p className="text-[3vw] pb-2 text-center">
+          fullstack web development, product discovery,
+          <br />
+          rapid prototyping, WebGL/GLSL, Unity
+        </p>
         <div className="max-w-[600px] divide-y divide-white/10 *:p-4 md:*:p-8 bg-white/10 backdrop-blur-md rounded-t-xl">
+          <div className="text-md leading-snug flex flex-col gap-4">
+            <div className="flex gap-2 text-sm">
+              <Image
+                src="/mark.png"
+                width={avatarSize}
+                height={avatarSize}
+                alt="Mark Miro"
+                className="shrink-0 rounded-full sepia-[.25]"
+              />
+              <p>
+                Mark is a creative JavaScript and React developer / designer
+                based in Sacramento, California. He’s worked for Bay Area
+                companies like Thumbtack and TrustToken (now Archblock).
+              </p>
+            </div>
+            <div className="flex gap-2 text-sm">
+              <Image
+                src="/george.jpeg"
+                width={avatarSize}
+                height={avatarSize}
+                alt="George Michel"
+                className="shrink-0 rounded-full sepia-[.25]"
+              />
+              <p>
+                Mark is a creative JavaScript and React developer / designer
+                based in Sacramento, California. He’s worked for Bay Area
+                companies like Thumbtack and TrustToken (now Archblock).
+              </p>
+            </div>
+          </div>
           <p className="text-md leading-snug">
             Mark Miro and George Michel started Hexaity Consulting out of a
             desire to bring about a new kind of consultancy. We were born out of
             Sacramento’s local rhizomatic milieu.
           </p>
-          <p className="text-sm text-white/50">
+          <p className="text-xs text-white/50">
             Haecceity (/hɛkˈsiːɪti, hiːk-/; from the Latin haecceitas, which
             translates as &ldquo;thisness&rdquo;) is a term from medieval
             scholastic philosophy, first coined by followers of Duns Scotus to
